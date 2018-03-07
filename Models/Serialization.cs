e81f073759a2c8e3262ec9f3f8f1a02d29030b57
Martin Komischke <martin.komischke@outlook.com>
2018-03-07 11:24:44 +0100

    add json

---
+++ Models/Serialization.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace Timetable.Models
+{
+    public static class Serialization
+    {
+        public static string ToJson(Table table)
+        {
+            return JsonConvert.SerializeObject(table);
+        }
+
+        public static Table FromJson(string json)
+        {
+            return JsonConvert.DeserializeObject<Table>(json);
+        }
+    }
+}
