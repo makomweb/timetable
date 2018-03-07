f106ff0dbd34ffede7f55f71c48db18572f30c4c
Martin Komischke <martin.komischke@outlook.com>
2018-03-07 12:35:44 +0100

    make serialization work

--- Models/Table.cs
+++ Models/Table.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
 
 namespace Timetable.Models
 {
@@ -26,15 +25,19 @@ namespace Timetable.Models
             Begin = begin;
             Duration = duration;
         }
-    }
 
+        public abstract string Type { get; }
+    }
+    
     public class Break : Item
     {
         public Break(DateTime begin, TimeSpan duration) : base(begin, duration)
         {
         }
-    }
 
+        public override string Type => nameof(Break);
+    }
+    
     public abstract class Block : Item
     {
         public Subject Subject { get; }
@@ -44,36 +47,31 @@ namespace Timetable.Models
             Subject = subject;
         }
     }
-
+    
     public class RegularBlock : Block
     {
         public RegularBlock(DateTime begin, string name) : this(begin, new Subject(name))
         {
         }
 
-        public RegularBlock(DateTime begin, Subject subject) : base(subject, begin, TimeSpan.FromMinutes(45))
+        public RegularBlock(DateTime begin, Subject subject) : base(subject, begin, DefaultDuration)
         {
         }
-    }
 
+        public static TimeSpan DefaultDuration = TimeSpan.FromMinutes(45);
+
+        public override string Type => nameof(RegularBlock);
+    }
+    
     public class DoubleBlock : Block
     {
-        public DoubleBlock(DateTime begin, Subject subject) : base(subject, begin, TimeSpan.FromMinutes(90))
+        public DoubleBlock(DateTime begin, Subject subject) : base(subject, begin, DefaultDuration)
         {
         }
-    }
 
-    public class DayColumn : DataColumn
-    {
-        public string Day => Caption;
+        public static TimeSpan DefaultDuration = TimeSpan.FromMinutes(90);
 
-        public IEnumerable<Item> Items { get; } = Enumerable.Empty<Item>();
-
-        public DayColumn(string day, IEnumerable<Item> items = null)
-        {
-            Caption = day;
-            Items = items;
-        }
+        public override string Type => nameof(DoubleBlock);
     }
 
     public class Table
@@ -82,14 +80,19 @@ namespace Timetable.Models
         
         public Table()
         {
-            _table.Columns.Add("Stunde", typeof(int));
-            _table.Columns.Add("Montag", typeof(string));
-            _table.Columns.Add("Dienstag", typeof(string));
-            _table.Columns.Add("Mittwoch", typeof(string));
-            _table.Columns.Add("Donnerstag", typeof(string));
-            _table.Columns.Add("Freitag", typeof(string));
-            _table.Rows.Add(1, "Deutsch", "Sport", "Mathe", "Deutsch", "Sachkunde");
-            _table.Rows.Add(2, "Musik", "Sport", "Sachkunde", "Mathe", "Englisch");
+            _table.Columns.Add("Tag", typeof(string));
+            _table.Columns.Add("1", typeof(Item));
+            _table.Columns.Add("2", typeof(Item));
+            _table.Columns.Add("3", typeof(Item));
+            _table.Columns.Add("4", typeof(Item));
+            _table.Columns.Add("5", typeof(Item));
+            _table.Columns.Add("6", typeof(Item));
+            _table.Columns.Add("7", typeof(Item));
+            _table.Columns.Add("8", typeof(Item));
+            //_table.Rows.Add(1, "Deutsch", "Sport", "Mathe", "Deutsch", "Sachkunde");
+            //_table.Rows.Add(2, "Musik", "Sport", "Sachkunde", "Mathe", "Englisch");
+
+            _table.Rows.Add("Montag", new RegularBlock(DateTime.Now, "Deutsch"), new Break(DateTime.Now.Add(RegularBlock.DefaultDuration), TimeSpan.FromMinutes(15)), new RegularBlock(DateTime.Now, "Sport"));
         }
 
         public string ToJson()
