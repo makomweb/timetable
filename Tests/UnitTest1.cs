128f86add7ec8dbf58789d9971e455aeb70250f2
Martin Komischke <martin.komischke@outlook.com>
2018-03-07 10:22:15 +0100

    add Tests project

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Timetable;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var obj = new Class1();
+            Assert.IsNotNull(obj);
+        }
+    }
+}
