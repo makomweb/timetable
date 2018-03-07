a38c0710ebe6eb10a89a5a436615ebf9b8344ff2
Martin Komischke <martin.komischke@outlook.com>
2018-03-07 11:29:19 +0100

    adjust test

---
+++ Tests/TableSerializationTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Timetable.Models;
+
+namespace Tests
+{
+    [TestClass]
+    public class TableSerializationTests
+    {
+        [TestMethod]
+        public void When_serializing_table_it_should_return_json()
+        {
+            var table = new Table();
+            var json = Serialization.ToJson(table);
+            Assert.IsFalse(string.IsNullOrEmpty(json), "JSON string should not be null or empty!");
+        }
+    }
+}
