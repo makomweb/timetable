75df49d05a01ae891a93ab32ce4779d6874a9448
Martin Komischke <martin.komischke@outlook.com>
2018-03-12 10:42:19 +0100

    move DayFactory from tests project to class lib

---
+++ DayFactory.cs
@@ -0,0 +1,48 @@
+﻿using Ninject;
+using System.Collections.Generic;
+
+namespace Timetable
+{
+    public class DayFactory
+    {
+        private readonly BlockFactory _blockFactory;
+        private readonly BreakFactory _breakFactory;
+
+        public static DayFactory Create(BlockStartTime blockStart)
+        {
+            var kernel = new StandardKernel();
+            kernel.Bind<BlockStartTime>().ToConstant(blockStart);
+            return kernel.Get<DayFactory>();
+        }
+
+        public DayFactory(BlockFactory blockFactory, BreakFactory breakFactory)
+        {
+            _blockFactory = blockFactory;
+            _breakFactory = breakFactory;
+        }
+
+        public object[] CreateDay(string name, params string[] subjects)
+        {
+            var items = new List<object> { name };
+
+            for (var i = 0; i < subjects.Length; i++)
+            {
+                var subject = subjects[i];
+                if (subject == null)
+                {
+                    continue;
+                }
+
+                items.Add(_blockFactory.CreateDoubleBlock(i, subject));
+
+                // Check if a break has to be appended (there is no break after the last block)
+                if (i < (subjects.Length - 1))
+                {
+                    items.Add(_breakFactory.BreakAfter(i));
+                }
+            }
+
+            return items.ToArray();
+        }
+    }
+}
--- Tests/TestTable.cs
+++ Tests/TestTable.cs
@@ -20,47 +20,7 @@ namespace Tests
         {
         }
 
-        public class DayFactory
         {
-            private readonly BlockFactory _blockFactory;
-            private readonly BreakFactory _breakFactory;
-
-            public static DayFactory Create(BlockStartTime blockStart)
-            {
-                var kernel = new StandardKernel();
-                kernel.Bind<BlockStartTime>().ToConstant(blockStart);
-                return kernel.Get<DayFactory>();
-            }
-
-            public DayFactory(BlockFactory blockFactory, BreakFactory breakFactory)
-            {
-                _blockFactory = blockFactory;
-                _breakFactory = breakFactory;
-            }
-
-            public object[] CreateDay(string name, params string[] subjects)
-            {
-                var items = new List<object> { name };
-
-                for (var i = 0; i < subjects.Length; i++)
-                {
-                    var subject = subjects[i];
-                    if (subject == null)
-                    {
-                        continue;
-                    }
-
-                    items.Add(_blockFactory.CreateDoubleBlock(i, subject));
-
-                    // Check if a break has to be appended (there is no break after the last block)
-                    if (i < (subjects.Length - 1))
-                    {
-                        items.Add(_breakFactory.BreakAfter(i));
-                    }
-                }
-
-                return items.ToArray();
-            }
         }
 
         private static DataTable CreateTable()
