8fd3b95fd886a890cf339e7543b3e1c100072cdd
Martin Komischke <martin.komischke@outlook.com>
2018-03-14 11:16:01 +0100

    convert table to http-response-message

--- WebApp/Controllers/JannisController.cs
+++ WebApp/Controllers/JannisController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
 using Timetable;
 
 namespace WebApp.Controllers
@@ -7,9 +11,22 @@ namespace WebApp.Controllers
     {
         private JannisTableA _table = new JannisTableA();
 
-        public Table Get()
+        public HttpResponseMessage Get()
         {
-            return _table;
+            return CreateMessage(_table);
+        }
+
+        private static HttpResponseMessage CreateMessage(JannisTableA table)
+        {
+            try
+            {
+                var json = table.ToJson();
+                return new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent(json, Encoding.UTF8, "application/json") };
+            }
+            catch (Exception ex)
+            {
+                return new HttpResponseMessage(HttpStatusCode.InternalServerError) { Content = new StringContent(ex.Message) };
+            }
         }
     }
 }
