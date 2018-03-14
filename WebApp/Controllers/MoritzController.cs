d0c2c2b2c1d8646a1dd34e3dd7d98637b4804395
Martin Komischke <martin.komischke@outlook.com>
2018-03-14 12:30:43 +0100

    add controllers for Moritz and Toni

---
+++ WebApp/Controllers/MoritzController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.Http;
+using Timetable;
+
+namespace WebApp.Controllers
+{
+    public class MoritzController : ApiController
+    {
+        private MoritzTable _table = new MoritzTable();
+
+        public HttpResponseMessage Get()
+        {
+            try
+            {
+                return JsonResponseMessage.From(_table);
+            }
+            catch (Exception ex)
+            {
+                return JsonResponseMessage.From(ex);
+            }
+        }
+    }
+}
---
+++ WebApp/Controllers/ToniController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.Http;
+using Timetable;
+
+namespace WebApp.Controllers
+{
+    public class ToniController : ApiController
+    {
+        private ToniTable _table = new ToniTable();
+
+        public HttpResponseMessage Get()
+        {
+            try
+            {
+                return JsonResponseMessage.From(_table);
+            }
+            catch (Exception ex)
+            {
+                return JsonResponseMessage.From(ex);
+            }
+        }
+    }
+}
