9cc94397a7ba63b7afee8befdfdc8ef049502414
Martin Komischke <martin.komischke@outlook.com>
2018-03-14 10:58:10 +0100

    add table controller

---
+++ WebApp/Controllers/JannisController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+using Timetable;
+
+namespace WebApp.Controllers
+{
+    public class JannisController : ApiController
+    {
+        private JannisTableA _table = new JannisTableA();
+
+        public Table Get()
+        {
+            return _table;
+        }
+    }
+}
