cc07585da818994dfc9e4519a4533720b26129c8
Martin Komischke <martin.komischke@outlook.com>
2018-03-14 16:53:08 +0100

    let JannisController expose ical

---
+++ WebApp/Controllers/CalendarContent.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http;
+using System.Text;
+using Timetable;
+
+namespace WebApp.Controllers
+{
+    public class CalendarContent : StringContent
+    {
+        public const string MediaType = "text/ical";
+
+        public static CalendarContent From(Table table)
+        {
+            var ics = table.ToIcs();
+            return new CalendarContent(ics);
+        }
+
+        protected CalendarContent(string raw) : base(raw, Encoding.UTF8, MediaType)
+        {
+        }
+    }
+}
---
+++ WebApp/Controllers/CalendarResponseMessage.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using System.Net.Http;
+using Timetable;
+
+namespace WebApp.Controllers
+{
+    public static class CalendarResponseMessage
+    {
+        public static HttpResponseMessage From(Table table)
+        {
+            return new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = CalendarContent.From(table)
+            };
+        }
+    }
+}
