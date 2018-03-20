67fdeca081033c8fa92fd1e30477a07cb6d45cbd
Martin Komischke <martin.komischke@outlook.com>
2018-03-20 16:17:40 +0100

    aggregate tables into a single calendar

---
+++ WebApp/Controllers/KidsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.Http;
+using Timetable;
+
+namespace WebApp.Controllers
+{
+    public class KidsController : ApiController
+    {
+#if true
+        private Table _jannis = new JannisTableA();
+#else
+        private Table _jannis = new JannisTableB();
+#endif
+        private Table _toni = new ToniTable();
+        private Table _moritz = new MoritzTable();
+
+        public HttpResponseMessage Get()
+        {
+            try
+            {
+                return CalendarResponseMessage.From(_jannis, _toni, _moritz);
+            }
+            catch (Exception ex)
+            {
+                return JsonResponseMessage.From(ex);
+            }
+        }
+    }
+}
