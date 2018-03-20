2a9c1e6938f33ec53bcebbc1b74daa3885efcfb8
Martin Komischke <martin.komischke@outlook.com>
2018-03-20 16:47:47 +0100

    calculate week start from UTC

--- Timetable/Week.cs
+++ Timetable/Week.cs
@@ -4,11 +4,11 @@ namespace Timetable
 {
     public static class Week
     { 
-        public static DateTime Start => GetStart(DateTime.Now, DayOfWeek.Monday);
+        public static DateTime Start => GetStart(DateTime.UtcNow, DayOfWeek.Monday);
 
         public static DateTime GetStart(DateTime now, DayOfWeek startOfWeek)
         {
-            Ensure.That(now.Kind == DateTimeKind.Local, $"Argument '{nameof(now)}' must be of type local time!");
+            Ensure.That(now.Kind == DateTimeKind.Utc, $"Argument '{nameof(now)}' must be of kind UTC!");
 
             int diff = (7 + (now.DayOfWeek - startOfWeek)) % 7;
             return now.AddDays(-1 * diff).Date;
