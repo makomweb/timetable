3ee3d0055442da29d81146369ec0b76042ad009d
Martin Komischke <martin.komischke@outlook.com>
2018-03-20 16:16:01 +0100

    different controllers for Jannis

--- WebApp/Controllers/JannisBController.cs
+++ WebApp/Controllers/JannisBController.cs
@@ -5,13 +5,9 @@ using Timetable;
 
 namespace WebApp.Controllers
 {
-    public class JannisController : ApiController
+    public class JannisBController : ApiController
     {
-#if false
-        private Table _table = new JannisTableA();
-#else
         private Table _table = new JannisTableB();
-#endif
 
         public HttpResponseMessage Get()
         {
