0b0cce86dc29a68c494099dbda9d4b8cc5a7453c
Martin Komischke <martin.komischke@outlook.com>
2018-03-22 09:19:27 +0100

    add exploring timezone test

---
+++ Tests/TimezoneTests.cs
@@ -0,0 +1,21 @@
+﻿using Ical.Net.DataTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tests
+{
+    [TestClass]
+    public class TimezoneTests
+    {
+        [TestMethod]
+        public void Run()
+        {
+            string timezoneId = "Europe/Berlin";
+            var lt = DateTime.UtcNow;
+            var dt = new CalDateTime(lt, timezoneId); // Note the UTC clock is carried into the local timezone!
+            var utc = dt.AsUtc;
+
+            Assert.IsTrue(utc.Hour < dt.Hour, "UTC is expected to be smaller then local time!");
+        }
+    }
+}
