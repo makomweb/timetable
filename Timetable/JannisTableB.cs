287495ab6a3ecdaa4da0fb9da1b795a0ea13ac2f
Martin Komischke <martin.komischke@outlook.com>
2018-04-09 08:37:39 +0200

    adjusted JannisTableB

--- Timetable/JannisTableB.cs
+++ Timetable/JannisTableB.cs
@@ -13,16 +13,16 @@ namespace Timetable
             var weekdays = new List<Weekday>();
 
             var dayFactory = DayFactory.Create(BlockStartTimes);
-            var monday = dayFactory.CreateWeekday("Monday", null, "Deutsch", "Englisch", "Kunst");
+            var monday = dayFactory.CreateWeekday("Monday", null, "Deutsch", "Englisch", "Ethik");
             weekdays.Add(monday);
 
-            var tuesday = dayFactory.CreateWeekday("Tuesday", "Französisch", "Informatik", "Mathe");
+            var tuesday = dayFactory.CreateWeekday("Tuesday", null, "Informatik", "Französich", "Mathe");
             weekdays.Add(tuesday);
 
             var wednesday = dayFactory.CreateWeekday("Wednesday", "Englisch", "Musik", "Sport", "Französisch");
             weekdays.Add(wednesday);
 
-            var thursday = dayFactory.CreateWeekday("Thursday", "Chemie/Physik", "Geschichte", "Ethik");
+            var thursday = dayFactory.CreateWeekday("Thursday", "Deutsch", "Chemie/Physik", "Geschichte", "Ethik");
             weekdays.Add(thursday);
 
             var friday = dayFactory.CreateWeekday("Friday", "Sport", "Biologie", "Mathematik");
