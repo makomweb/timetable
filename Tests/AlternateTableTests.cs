4b26ed878188df3d6c7042936182bf4bd3c3a5fd
Martin Komischke <martin.komischke@outlook.com>
2018-04-17 13:56:48 +0200

    be explicit about UTC vs. local time

--- Tests/AlternateTableTests.cs
+++ Tests/AlternateTableTests.cs
@@ -7,7 +7,7 @@ namespace Tests
     [TestClass]
     public class AlternateTableTests
     {
-        private static DateTime _startDate = new DateTime(2018, 4, 11);
+        private static DateTime _startDate = new DateTime(2018, 4, 11, 0, 0, 1, DateTimeKind.Utc);
         private static JannisTableA _startTable = new JannisTableA();
         private static JannisTableB _alternateTable = new JannisTableB();
         private AlternateTable _table = new AlternateTable(_startDate, _startTable, _alternateTable);
@@ -15,7 +15,7 @@ namespace Tests
         [TestMethod]
         public void Same_week_should_show_same_table()
         {
-            var table = _table.Get(new DateTime(2018, 4, 13));
+            var table = _table.Get(new DateTime(2018, 4, 13, 0, 0, 1, DateTimeKind.Utc));
 
             Assert.IsInstanceOfType(table, typeof(JannisTableA), "Table is not of expected type JannisTableA!");
         }
@@ -23,7 +23,7 @@ namespace Tests
         [TestMethod]
         public void Next_week_should_show_next_table()
         {
-            var table = _table.Get(new DateTime(2018, 4, 17));
+            var table = _table.Get(new DateTime(2018, 4, 17, 0, 0, 1, DateTimeKind.Utc));
 
             Assert.IsInstanceOfType(table, typeof(JannisTableB), "Table is not of expected type JannisTableB!");
         }
@@ -31,7 +31,7 @@ namespace Tests
         [TestMethod]
         public void Week_after_next_week_should_show_initial_table()
         {
-            var table = _table.Get(new DateTime(2018, 4, 25));
+            var table = _table.Get(new DateTime(2018, 4, 25, 0, 0, 1, DateTimeKind.Utc));
 
             Assert.IsInstanceOfType(table, typeof(JannisTableA), "Table is not of expected type JannisTableA!");
         }
