c994188eee152e7641366b6798dd711753891310
Martin Komischke <martin.komischke@outlook.com>
2018-04-17 13:48:05 +0200

    implement class to provide alternate table depending on the specified date

---
+++ Tests/AlternateTableTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Timetable;
+
+namespace Tests
+{
+    [TestClass]
+    public class AlternateTableTests
+    {
+        private static DateTime _startDate = new DateTime(2018, 4, 11);
+        private static JannisTableA _startTable = new JannisTableA();
+        private static JannisTableB _alternateTable = new JannisTableB();
+        private AlternateTable _table = new AlternateTable(_startDate, _startTable, _alternateTable);
+
+        [TestMethod]
+        public void Same_week_should_show_same_table()
+        {
+            var table = _table.Get(new DateTime(2018, 4, 13));
+
+            Assert.IsInstanceOfType(table, typeof(JannisTableA), "Table is not of expected type JannisTableA!");
+        }
+
+        [TestMethod]
+        public void Next_week_should_show_next_table()
+        {
+            var table = _table.Get(new DateTime(2018, 4, 17));
+
+            Assert.IsInstanceOfType(table, typeof(JannisTableB), "Table is not of expected type JannisTableB!");
+        }
+
+        [TestMethod]
+        public void Week_after_next_week_should_show_initial_table()
+        {
+            var table = _table.Get(new DateTime(2018, 4, 25));
+
+            Assert.IsInstanceOfType(table, typeof(JannisTableA), "Table is not of expected type JannisTableA!");
+        }
+    }
+}
---
+++ Timetable/AlternateTable.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Globalization;
+
+namespace Timetable
+{
+    public class AlternateTable
+    {
+        private readonly DateTime _startDate;
+        private readonly Table _startTable;
+        private readonly Table _alternateTable;
+
+        public AlternateTable(DateTime startDate, Table startTable, Table alternateTable)
+        {
+            _startDate = startDate;
+            _startTable = startTable;
+            _alternateTable = alternateTable;
+        }
+
+        public Table Get(DateTime date)
+        {
+            var week = new WeekOfYear();
+
+            var diff = week.GetNumber(date) - week.GetNumber(_startDate);
+
+            if (IsOdd(diff))
+            {
+                return _alternateTable;
+            }
+            else
+            {
+                return _startTable;
+            }
+        }
+
+        private static bool IsOdd(int number)
+        {
+            return number % 2 != 0;
+        }
+
+        private class WeekOfYear
+        {
+            private readonly DateTimeFormatInfo _info;
+
+            public WeekOfYear() : this(DateTimeFormatInfo.CurrentInfo)
+            {
+            }
+
+            public WeekOfYear(DateTimeFormatInfo info)
+            {
+                _info = info;
+            }
+
+            public int GetNumber(DateTime date)
+            {
+                var calendar = _info.Calendar;
+                return calendar.GetWeekOfYear(date, _info.CalendarWeekRule, _info.FirstDayOfWeek);
+
+            }
+        }
+    }
+}
