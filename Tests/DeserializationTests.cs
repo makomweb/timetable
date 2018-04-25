d20f57dcb98351dfcc960f1980f7578e8c8ef7da
Martin Komischke <martin.komischke@outlook.com>
2018-04-25 09:41:44 +0200

    deserialize blocks

--- Tests/DeserializationTests.cs
+++ Tests/DeserializationTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
+using System;
 using System.IO;
 using System.Linq;
 using Timetable;
@@ -10,10 +11,25 @@ namespace Tests
     public class DeserializationTests
     {
         [TestMethod]
-        public void Deserialize_table_from_file_should_succeed()
+        public void When_deserializing_table_should_be_available()
         {
             var table = Deserialize("TimeTable.json");
             Assert.IsNotNull(table, "table should not be null!");
+        }
+
+        [TestMethod]
+        public void When_deserializing_block_start_time_should_be_available()
+        {
+            var table = Deserialize("TimeTable.json");
+            Assert.IsNotNull(table.StartTimes, "Block start times should not be null!");
+        }
+
+        [TestMethod]
+        public void When_deserializing_block_should_contain_subject_name()
+        {
+            var table = Deserialize("TimeTable.json");
+            var block = table.Weekdays.First().Blocks.First();
+            Assert.IsFalse(string.IsNullOrEmpty(block.Name), "Block should contain subject name!");
         }
 
         private TestTable Deserialize(string fileName)
@@ -23,23 +39,27 @@ namespace Tests
             return JsonConvert.DeserializeObject<TestTable>(json);
         }
 
-        private class TestWeekday : Weekday
+        private class TestBlock
         {
-            [JsonProperty("Name")]
-            public string DayName { get; set; }
+            public string Name { get; set; }
+            
+            public string Begin { get; set; }
         }
 
-        private class TestTable : Table
+        private class TestWeekday
         {
-            public TestTable() : base(Enumerable.Empty<TestWeekday>())
-            {
-            }
+            public string Name { get; set; }
 
-            public TestWeekday[] Weekdays
-            {
-                get { return Days.Cast<TestWeekday>().ToArray(); }
-                set { Days = value; }
-            }
+            public TestBlock[] Blocks { get; set; }
+        }
+
+        private class TestTable
+        {
+            public string[] BlockStartTimes { get; set; }
+
+            public BlockStartTime StartTimes => new BlockStartTime(BlockStartTimes);
+
+            public TestWeekday[] Weekdays { get; set; }
         }
     }
 }
