44c95ba700099f70a98b89289080bbadd3a5c209
Martin Komischke <martin.komischke@outlook.com>
2018-04-25 10:12:25 +0200

    implement abstract factory for blocks

--- Timetable/Block.cs
+++ Timetable/Block.cs
@@ -12,6 +12,21 @@ namespace Timetable
             Subject = subject;
         }
 
+        public static Block Create(TimeSpan begin, string subject, string blockType)
+        {
+            return Create(begin, new Subject(subject), blockType);
+        }
+
+        public static Block Create(TimeSpan begin, Subject subject, string blockType)
+        {
+            switch (blockType)
+            {
+                case "RegularBlock": return new RegularBlock(begin, subject);
+                case "DoubleBlock": return new DoubleBlock(begin, subject);
+                default: throw new NotSupportedException($"Blocktype '{blockType}' is not supported!");
+            }
+        }
+
         public CalendarEvent ToCalendarEvent(DateTime date, string timezoneId)
         {
             var begin = GetStartTime(date, timezoneId);
