35705e6d91899c0f81ec3d034dd2e3fa54e01b82
Martin Komischke <martin.komischke@outlook.com>
2018-04-25 08:47:27 +0200

    implement table deserialization from JSON

---
+++ Tests/DeserializationTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System.IO;
+using System.Linq;
+using Timetable;
+
+namespace Tests
+{
+    [TestClass]
+    public class DeserializationTests
+    {
+        [TestMethod]
+        public void Deserialize_table_from_file_should_succeed()
+        {
+            var table = Deserialize("TimeTable.json");
+            Assert.IsNotNull(table, "table should not be null!");
+        }
+
+        private TestTable Deserialize(string fileName)
+        {
+            var path = Path.Combine("Data", fileName);
+            var json = File.ReadAllText(path);
+            return JsonConvert.DeserializeObject<TestTable>(json);
+        }
+
+        private class TestTable : Table
+        {
+            public TestTable() : base(Enumerable.Empty<Weekday>())
+            {
+            }
+        }
+    }
+}
