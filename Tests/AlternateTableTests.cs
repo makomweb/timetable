2d75fdd5381aa103855c625a4e7d984cccb14b23
Martin Komischke <martin.komischke@outlook.com>
2018-09-06 08:50:50 +0200

    use better way to initialize test

--- Tests/AlternateTableTests.cs
+++ Tests/AlternateTableTests.cs
@@ -7,15 +7,21 @@ namespace Tests
     [TestClass]
     public class AlternateTableTests
     {
-        private static DateTime _startDate = new DateTime(2018, 8, 27, 0, 0, 1, DateTimeKind.Utc);
-        private static JannisTableA _startTable = new JannisTableA();
-        private static JannisTableB _alternateTable = new JannisTableB();
-        private WeeklyAlternateTable _table = new WeeklyAlternateTable(_startDate, _startTable, _alternateTable);
+        private WeeklyAlternateTable _table;
+
+        public AlternateTableTests()
+        {
+            var startDate = new DateTime(2018, 8, 27, 0, 0, 1, DateTimeKind.Utc);
+            var startTable = new JannisTableA();
+            var alternateTable = new JannisTableB();
+            _table = new WeeklyAlternateTable(startDate, startTable, alternateTable);
+        }
 
         [TestMethod]
         public void Same_week_should_show_same_table()
         {
-            var table = _table.Get(new DateTime(2018, 8, 29, 0, 0, 1, DateTimeKind.Utc));
+            var dateSameWeek = new DateTime(2018, 8, 29, 0, 0, 1, DateTimeKind.Utc);
+            var table = _table.Get(dateSameWeek);
 
             Assert.IsInstanceOfType(table, typeof(JannisTableA), "Table is not of expected type JannisTableA!");
         }
@@ -23,7 +29,8 @@ namespace Tests
         [TestMethod]
         public void Next_week_should_show_next_table()
         {
-            var table = _table.Get(new DateTime(2018, 9, 5, 0, 0, 1, DateTimeKind.Utc));
+            var dateNextWeek = new DateTime(2018, 9, 5, 0, 0, 1, DateTimeKind.Utc);
+            var table = _table.Get(dateNextWeek);
 
             Assert.IsInstanceOfType(table, typeof(JannisTableB), "Table is not of expected type JannisTableB!");
         }
@@ -31,7 +38,8 @@ namespace Tests
         [TestMethod]
         public void Week_after_next_week_should_show_initial_table()
         {
-            var table = _table.Get(new DateTime(2018, 9, 11, 0, 0, 1, DateTimeKind.Utc));
+            var dateWeekAfterNextWeek = new DateTime(2018, 9, 11, 0, 0, 1, DateTimeKind.Utc);
+            var table = _table.Get(dateWeekAfterNextWeek);
 
             Assert.IsInstanceOfType(table, typeof(JannisTableA), "Table is not of expected type JannisTableA!");
         }
